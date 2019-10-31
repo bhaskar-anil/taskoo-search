06e78ccd9e7f4f875f27911eb8087b3019680d51
Bhaskar <anil.bhaskar@rinmac287.intra.rakuten.co.jp>
2019-10-31 13:55:04 +0530

    siri ganesh

---
+++ src/main/java/in/taskoo/search/TaskooSearchApplication.java
@@ -0,0 +1,13 @@
+package in.taskoo.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskooSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskooSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/taskoo/search/TaskooSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package in.taskoo.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskooSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
